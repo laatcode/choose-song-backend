9a5d2d13a3db15207d2da18c6f1567c6a8e6c5f9
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-02-06 14:56:25 -0500

    Making ArtistController methods static

--- modules/artist/artist.controller.js
+++ modules/artist/artist.controller.js
@@ -3,11 +3,10 @@ const {v4: uuid} = require('uuid')
 const CustomError = require('../../CustomError')
 
 class ArtistController {
-    constructor() {
-        this.artists = []
-    }
 
-    seeds() {
+    static artists = []
+
+    static seeds() {
         for (let i = 0; i < 100; i++) {
             this.artists.push({
                 id: faker.string.uuid(),
@@ -16,11 +15,11 @@ class ArtistController {
         }
     }
 
-    find() {
+    static find() {
         return this.artists
     }
 
-    findIndex(id) {
+    static findIndex(id) {
         const index = this.artists.findIndex(artist => artist.id === id)
 
         if(index === -1) {
@@ -30,11 +29,11 @@ class ArtistController {
         return index
     }
 
-    findOne(id) {
+    static findOne(id) {
         return this.artists[this.findIndex(id)]
     }
 
-    create(data) {
+    static create(data) {
         const newArtist = {
             ...data,
             id: uuid()
@@ -45,7 +44,7 @@ class ArtistController {
         return newArtist
     }
 
-    update(id, data) {
+    static update(id, data) {
         const foundArtist = this.findOne(id)
 
         const artistUpdated = {
@@ -60,7 +59,7 @@ class ArtistController {
         return artistUpdated
     }
 
-    delete(id) {
+    static delete(id) {
         this.findOne(id)
         this.artists = this.artists.filter(artist => artist.id !== id)
     }
--- modules/artist/artist.routes.js
+++ modules/artist/artist.routes.js
@@ -2,23 +2,22 @@ const router = require('express').Router()
 const validatorHandler = require('../../middlewares/validatorHandler')
 const artistSchemas = require('./artist.schema')
 const ArtistController = require('./artist.controller')
-const artist = new ArtistController()
 
 router.get('/seeds', (req, res) => {
-    artist.seeds()
+    ArtistController.seeds()
     res.send('Seeds created')
 })
 
-router.get('/', (req, res) => res.json(artist.find()))
+router.get('/', (req, res) => res.json(ArtistController.find()))
 
-router.get('/:id', validatorHandler(artistSchemas.getArtistSchema, 'params'), (req, res) => res.json(artist.findOne(req.params.id)))
+router.get('/:id', validatorHandler(artistSchemas.getArtistSchema, 'params'), (req, res) => res.json(ArtistController.findOne(req.params.id)))
 
-router.post('/', validatorHandler(artistSchemas.createArtistSchema, 'body'), (req, res) => res.status(201).json(artist.create(req.body)))
+router.post('/', validatorHandler(artistSchemas.createArtistSchema, 'body'), (req, res) => res.status(201).json(ArtistController.create(req.body)))
 
-router.patch('/:id', validatorHandler(artistSchemas.getArtistSchema, 'params'), validatorHandler(artistSchemas.updateArtistSchema, 'body'), (req, res) => res.json(artist.update(req.params.id, req.body)))
+router.patch('/:id', validatorHandler(artistSchemas.getArtistSchema, 'params'), validatorHandler(artistSchemas.updateArtistSchema, 'body'), (req, res) => res.json(ArtistController.update(req.params.id, req.body)))
 
 router.delete('/:id', validatorHandler(artistSchemas.getArtistSchema, 'params'), (req, res) => {
-    artist.delete(req.params.id)
+    ArtistController.delete(req.params.id)
     res.status(204).end()
 })
 
